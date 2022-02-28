eaa4653ba897273e7e1fcf2ce812a9eef47f8f70
Robin Rees <97979531+RobinRees@users.noreply.github.com>
2022-02-28 14:06:28 +0100

    Documents, database and Html started
    Löst alla document som jag kommer använda. Skapat en databas. Och valt att använda filmer som Steven Segall har haft en huvudroll i.

---
+++ database.js
@@ -0,0 +1,72 @@
+"use strict"
+// Movies Steven segall starred in
+let database = [
+    {
+        id: 1,
+        title: "On Deadly Ground",
+        director: "Steven Seagal",
+        imdbScore: 4.6,
+        releaseYear: 1994,
+    },
+    {
+        id: 2,
+        title: "Above the law",
+        director: "Andrew Davis",
+        imdbScore: 6.0,
+        releaseYear: 1988,
+    },
+    {
+        id: 3,
+        title: "Under Siege",
+        director: "Andrew Davis",
+        imdbScore: 6.5,
+        releaseYear: 1992,
+    },
+    {
+        id: 4,
+        title: "Exit Wounds",
+        director: "Andrzej Bartkowiak",
+        imdbScore: 5.6,
+        releaseYear: 2001,
+    },    {
+        id: 5,
+        title: "Marked for Death",
+        director: "Dwight H. Little",
+        imdbScore: 6.0,
+        releaseYear: 1990,
+    },
+    {
+        id: 6,
+        title: "Hard to Kill",
+        director: "Bruce Malmuth",
+        imdbScore: 5.8,
+        releaseYear: 1990,
+    },
+    {
+        id: 7,
+        title: "Renegade Justice",
+        director: "Don E. FauntLeRoy",
+        imdbScore: 5.2,
+        releaseYear: 2007,
+    },
+    {
+        id: 8,
+        title: "Half Past Dead",
+        director: "Don Michael Paul",
+        imdbScore: 4.7,
+        releaseYear: 2002,
+    },
+    {
+        id: 9,
+        title: "Attack Force",
+        director: "Michael Keusch",
+        imdbScore: 2.8,
+        releaseYear: 2006,
+    },    {
+        id: 10,
+        title: "Against the Dark",
+        director: "Richard Crudo",
+        imdbScore: 3.1,
+        releaseYear: 2009,
+    },
+];
