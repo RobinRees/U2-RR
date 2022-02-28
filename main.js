e71811dfa2e34db2724e756acbd3f7447cd1f431
Robin Rees <97979531+RobinRees@users.noreply.github.com>
2022-02-28 16:23:21 +0100

    Added some CSS and a function that renders the array into the site

--- main.js
+++ main.js
@@ -11,3 +11,44 @@ function createNewMovie (title, director, imdbScore, year) {
     };
     return movie;
 }
+
+function addMovieToDatabase(database, movie) {
+    database.push(movie);
+}
+
+
+function renderMovie(movie) {
+    let div = document.createElement("div"); //Deklarerar div och säger att den ska skapa ett "div" element i HTML
+    div.classList.add("movie"); //diven som skapas får classen: "movie"
+    div.id = movie.id; // diven som skapas får obejetet .id
+
+
+    //den div vi skapar får följande HTML kod i sig. ${movie.X} betyder att det ska stå vad det gör i just det minnet av arrayen.
+    div.innerHTML = ` 
+    <div>${movie.title}</div>
+    <div>${movie.director}</div>
+    <div>${movie.imdbRating}</div>
+    <div>${movie.releaseYear}</div>
+    <button type="button">Remove</button>
+    `;
+    return div;
+}
+
+function renderMovies(movies) {
+
+    let moviesElement = document.getElementById("movies");
+    moviesElement.innerHTML = "";
+
+
+    for (let movie of movies) {
+        let movieElement = renderMovie(movie);
+        moviesElement.appendChild(movieElement);
+    }
+
+
+
+
+}
+
+renderMovies(database)
+
